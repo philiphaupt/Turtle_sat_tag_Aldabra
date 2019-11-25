1478db477d665bbaeb4177f4109aadf3b476111e
Philip Haupt <philip.haupt@gmail.com>
2019-11-25 21:46:06 +0000

    add administrative boundaries for countries of interest

--- scripts/12_read_in_mpa_boundaries.R
+++ scripts/12_read_in_mpa_boundaries.R
@@ -3,6 +3,7 @@ library(sf)
 library(wdpar)
 library(tidyverse)
 library(ggmap)
+library(lwgeom)
 
 
 # Add protected areas to map through live link to https://www.protectedplanet.net/marine
@@ -51,7 +52,7 @@ shps_clean <- purrr::map(.x = shps[2:8], .f = wdpa_clean) #error message
 list2env(shps_clean,globalenv()) # The objects in the list have to be named to work!
 list2env(shps[1],globalenv())
 
-plot(st_geometry())
+#plot(st_geometry())
 
 
 # union - needs fixing - somthing not quite right
@@ -63,3 +64,41 @@ pa_aoi <- st_union(Tanzania, Mozambique, Comoros,Kenya, Seychelles, Mauritius,  
 
 # plot data
 plot(st_geometry(pa_aoi))
+
+## countries are in the input folder
+gadm_dir <- "E:/stats/aldabra/turtles/turtles_ald_sat_tag_2011_2014/Turtle_sat_tag_Aldabra/data/gadm_countries/"
+gadm_files <- list.files(gadm_dir)
+
+gadms <- list()
+# gadms_union <- as_Spatial()
+for (i in seq_along(gadm_files)) {
+        gadms[i] <- read_rds(paste0(gadm_dir, gadm_files[i])) %>% 
+                as_Spatial() %>% 
+                st_as_sf %>% 
+                st_set_precision(1000) %>%
+                lwgeom::st_make_valid() %>%
+                st_set_precision(1000) %>%
+                st_combine() %>%
+                st_union() %>%
+                st_set_precision(1000) %>%
+                lwgeom::st_make_valid() %>%
+                st_transform(st_crs(Seychelles)) %>%
+                lwgeom::st_make_valid()
+        # gadms_union <- sf::st_union(gadms[i],gadms_union)
+                
+}
+
+gadms_union <- map(gadms, st_union)
+gadms_union <- st_union(gamds)
+
+
+#
+# clip Malta's protected areas to the coastline
+# mlt_pa_data <- mlt_pa_data %>%
+#         filter(MARINE == "terrestrial") %>%
+#         st_intersection(mlt_boundary_data) %>%
+#         rbind(mlt_pa_data %>%
+#                       filter(MARINE == "marine") %>%
+#                       st_difference(mlt_boundary_data)) %>%
+#         rbind(mlt_pa_data %>% filter(!MARINE %in% c("terrestrial",
+#                                                     "marine")))
