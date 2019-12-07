32618e9dc3f90a6844e33f44c4a4d8dc6ddb959a
Philip Haupt <philip.haupt@gmail.com>
2019-12-07 07:12:41 +0000

    add detail

--- scripts/999_map_of_aldabra.R
+++ scripts/999_map_of_aldabra.R
@@ -3,6 +3,9 @@
 library(sf)
 library(tmap)
 library(sp)
+library(ggplot2)
+library(spData)
+
 
 # load simplified Aldabra
 ald_dir <-
@@ -11,22 +14,26 @@ ald_dir <-
 aldabra <- read_sf(paste0(ald_dir, "base.shp"))
 st_crs(aldabra) # is already in utm38s
 
-# Settlement <- st_point(c(896100,634600))
-# SpatialPolygons(list(Polygons(list(Polygon(Settlement)), 1))) %>%
-#         as(., "SpatialPolygonsDataFrame")
-# 
-# st_crs(Settlement)
-# Settlement <- Settlement %>% st_set_crs(st_crs(aldabra))
-# class(Settlement)
-# plot(Settlement)
+localities <- st_sfc(st_point(c(46.207,-9.4)), crs = 4326) #896100,634600
+localities <- st_sf(localities, name="Settlement")
+st_crs(localities)
+localities_utm38s <- localities %>% st_transform(st_crs(aldabra))#st_crs(aldabra)
 
 tmap_mode("plot") 
-tmap::tm_shape(aldabra) +
-        #tm_graticules()+ #gives decimal degrees
-        tm_grid()+ # will give utm
-        tm_polygons(lwd = 0.7)+
-        tm_layout(inner.margins = c(0.1,0.05)) +
-        tm_scale_bar(breaks = c(0,5, 10), text.size = 1) 
-        #tm_shape(Settlement)+
-        #tm_symbols("black")
+aldabra_map <- tmap::tm_shape(aldabra) +
+        tm_graticules(col = "gray87") + #gives decimal degrees
+        #tm_grid()+ # will give utm
+        tm_polygons(lwd = 0.7) +
+        tm_shape(localities) +
+        tm_symbols(col = "black",
+                   size = 0.5) +
+        #tm_legend("name")+
+        tm_text("name",
+                just = "left",
+                ymod = 0.9) +
+        tm_layout(inner.margins = c(0.1, 0.05)) +
+        tm_scale_bar(breaks = c(0, 5, 10), text.size = 1)
         
+        
+        # 
+tmap_save(aldabra_map, "./aldabra_map.png", width = 15, height = 7.5, units = "cm", dpi = 500)
